4875cba29ce5819a54a19ae84a27cb942988e926
Matt Langlois <fletchto99@gmail.com>
2017-10-25 10:58:08 -0400

    Some changes to reverse-1 challenge

--- reverse/reverse-1/solution/obfuscate.js
+++ reverse/reverse-1/solution/obfuscate.js
@@ -13,4 +13,6 @@ let mapping = "flag{wh4T_1S_jsFuck}".split('').forEach(chr => {
     })
 });
 
-fs.writeFileSync('./challenge', JScrewIt.encode(`eval("console.log('getting there!! Keep going!!');console.log('running... (about 5 seconds)');/* Try to find the flag, it should be hiding in here somewhere according to our reliable source! *//${data}/.source.replace(/.{7}/g,function(w){/* flag += */String.fromCharCode(parseInt(w.replace(/ /g,'0').replace(/	/g,'1'),2))});setTimeout(()=>{console.log('eval done... result: fail')},3000);")`));
+fs.writeFileSync('./challenge', JScrewIt.encode(`console.log('getting there!! Keep going!!');console.log('running... (about 5 seconds)');/* Try to find the flag, it should be hiding in here somewhere according to our reliable source! *//${data}/.source.replace(/.{7}/g,function(w){/* flag += */String.fromCharCode(parseInt(w.replace(/ /g,'0').replace(/	/g,'1'),2))});setTimeout(()=>{console.log('eval done... result: fail')},3000);`, {
+	runAs: 'call'
+}));
